fb6db0f1ac5234a57ca68f20fa82b901eac63bd8
mdrabbi97324 <mdrabbi97324@gmail.com>
2023-09-02 20:17:47 +0600

    Solve a problem in  inchToFeet

--- Js_apply_05/inchToFeet.js
+++ Js_apply_05/inchToFeet.js
@@ -27,21 +27,43 @@
 // console.log('Nana Feet :', nanaFeet);
 
 
-var heights = [ 150, 123, 33];
-
-console.log();
-
-function convertInchsToFeets(heights) {
-    
-    return heights;
-}
 
 
 // function height(num1, num2, num3) {
-//     let feet = num1 / 12;
-//     return feet;
+    //     let feet = num1 / 12;
+    //     return feet;
     
-// }
+    // }
+    
+    // console.log(height(heights));
+    
+    /* var heights = [ 150, 123, 33];
+    
+    console.log();
+    
+    function convertInchsToFeets(heights) {
+        
+        return heights;
+    } */
 
-// console.log(height(heights));
-  
+
+
+    function inchesToFeet(inchesArray) {
+        //Creating a new array to store the values in feet
+        var feetArray = [];
+        // Iterate through the input array
+        for (var i = 0; i<inchesArray.length; i++) {
+            //Convert inches to feet (1 foot = 12 inches)
+            var feetValue = inchesArray[i] / 12;
+
+            //Push the result into the feetArray
+            feetArray.push(feetValue);
+        }
+
+        return feetArray;
+    }
+
+    var inchesValues = [150, 123, 33]; // An array with values in inches
+    var feetValues = inchesToFeet(inchesValues);
+
+    console.log(feetValues);     
